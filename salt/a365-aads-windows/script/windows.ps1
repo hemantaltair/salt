2f80e6aed09d4fed815b34a9224b1816e1eaba3c
hemant <hemant.kumar2@weyv.com>
2019-11-12 16:27:47 +0530

    test

--- salt/a365-aads-windows/script/windows.ps1
+++ salt/a365-aads-windows/script/windows.ps1
@@ -1,5 +1,5 @@
 $userName = "hemank@altairunlimited.com"
-$userPassword = "xxxxx"
+$userPassword = {{ salt['pillar.get']('secrets:hemant:password') }}
 $secureStringPwd = $userPassword | ConvertTo-SecureString -AsPlainText -Force
 $credentials = New-Object System.Management.Automation.PSCredential -ArgumentList $userName, $secureStringPwd
 
