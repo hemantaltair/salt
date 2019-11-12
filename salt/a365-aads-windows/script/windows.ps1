223af311a43c4f1a6cd77d283d36b8ab2d550184
hemant <hemant.kumar2@weyv.com>
2019-11-12 18:02:16 +0530

    test

--- salt/a365-aads-windows/script/windows.ps1
+++ salt/a365-aads-windows/script/windows.ps1
@@ -3,4 +3,4 @@ $userPassword = "altair@12345"
 $secureStringPwd = $userPassword | ConvertTo-SecureString -AsPlainText -Force
 $credentials = New-Object System.Management.Automation.PSCredential -ArgumentList $userName, $secureStringPwd
 
-Add-Computer -DomainName {{ salt['pillar.get']('hostsfile:azure_ad_domain_name') }} -Credential $credentials -Retart -Force
+Add-Computer -DomainName {{ salt['pillar.get']('hostsfile:azure_ad_domain_name') }} -Credential $credentials -Restart -Force
